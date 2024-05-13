eafecf4542da599fd74d05fc491c3d2d24379bf1
Sergio Ruiz <sergioeruiz@hotmail.com>
2024-05-13 18:12:17 -0300

    Progress till lesson 25. Markdown chat display

--- pages/api/chat/sendMessage.js
+++ pages/api/chat/sendMessage.js
@@ -7,6 +7,11 @@ export const config = {
 export default async function handler(req) {
     try {
         const { message } = await req.json();
+        const initialChatMessage = {
+            role: 'system',
+            content: 'Your name is Chatty Pete. An incredibly intelligent and quick-thinking AI, taht always replies with a enthusiastic and positive energy. Your reponse must be formatted as markdown.',
+        };
+
         const stream = await new OpenAIEdgeStream(
             "https://api.openai.com/v1/chat/completions",
             {
@@ -17,10 +22,12 @@ export default async function handler(req) {
                 method: 'POST',
                 body: JSON.stringify({
                     model: 'gpt-3.5-turbo',
-                    messages: [{
-                        content: message,
-                        role: 'user', 
-                    }],
+                    messages: [initialChatMessage,
+                        {
+                            content: message,
+                            role: 'user', 
+                        }
+                    ],
                 }),
             }
         );
--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -9,9 +9,11 @@ export default function ChatPage() {
   const [ incomingMessage, setInconmingMessage ] = useState('');
   const [messageText, setMessageText] = useState("");
   const [newChatMessages,setNewChatMessages] = useState([]);
+  const [ generatingResponse, setGeneratingResponse ] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setGeneratingResponse(true);
     setNewChatMessages(prev => {
       const newChatMessages = [
         ...prev, {
@@ -22,6 +24,7 @@ export default function ChatPage() {
       ];
       return newChatMessages;
     });
+    setMessageText("");
     console.log("message text", newChatMessages);
     const response = await fetch(`/api/chat/sendMessage`, {
       method: 'POST',
@@ -42,6 +45,7 @@ export default function ChatPage() {
       console.log(message);
       setInconmingMessage(s => `${s}${message.content}`);
     });
+    setGeneratingResponse(false);
   };
 
   console.log(newChatMessages);
@@ -53,8 +57,8 @@ export default function ChatPage() {
       </Head>
       <div className="grid h-screen grid-cols-[260px_1fr]">
         <ChatSidebar />
-        <div className="bg-gray-700 text-white flex flex-col ">
-          <div className="flex-1">
+        <div className="bg-gray-700 text-white flex flex-col overflow-hidden">
+          <div className="flex-1 overflow-scroll">
             {newChatMessages.map(message => (
               <Message
                 key={message.id}
@@ -72,11 +76,11 @@ export default function ChatPage() {
           </div>
           <footer className="bg-gray-800 p-5">
             <form onSubmit={handleSubmit}>
-              <fieldset className="flex gap-2">
+              <fieldset className="flex gap-2" disabled={generatingResponse}>
                 <textarea
                   value={ messageText }
                   onChange={(e) => setMessageText(e.target.value)}
-                  placeholder="Send a message.."
+                  placeholder={ generatingResponse ? '' : 'Send a message..'}
                   className="w-full resize-none rounded-md bg-gray-700 p-2 focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500" />
                 <button type="submit" className="btn">Send</button>
               </fieldset>
